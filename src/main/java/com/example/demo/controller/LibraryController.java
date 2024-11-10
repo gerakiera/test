2fa8ce9b6082b680f4cac51c2bb451bcac79aca0
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-11-10 23:24:57 +0300

    add PreAuthorize

--- src/main/java/com/example/demo/controller/LibraryController.java
+++ src/main/java/com/example/demo/controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.Reader;
 import com.example.demo.model.Transaction;
 import com.example.demo.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +22,20 @@ public class LibraryController {
     private LibraryService libraryService;
 
     @PostMapping("/transaction")
+    @PreAuthorize("isAuthenticated()")
     public Transaction executionTransaction(@RequestParam Long bookId, @RequestParam Long readerId,
                                             @RequestParam String transactionType) {
         return libraryService.executionTransaction(bookId, readerId, transactionType);
     }
 
     @GetMapping("/popular-author")
+    @PreAuthorize("isAuthenticated()")
     public Author getMostPopularAuthor(@RequestParam LocalDateTime startDate, @RequestParam LocalDateTime endDate) {
         return libraryService.getMostPopularAuthor(startDate, endDate);
     }
 
     @GetMapping("/most-active-reader")
+    @PreAuthorize("isAuthenticated()")
     public Reader getMostActiveReader() {
         return libraryService.getMostActiveReader();
     }
