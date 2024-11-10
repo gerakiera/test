4e2e8b5328f3e3316f14c659263e1b40c0e69abc
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-11-10 23:15:55 +0300

    add service implements UserDetailsManager

---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService implements UserDetailsManager {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public void createUser(UserDetails userData) {
+        var user = new User();
+        user.setLogin(userData.getUsername());
+        var hashedPassword = passwordEncoder.encode(userData.getPassword());
+        user.setPasswordDigest(hashedPassword);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'userExists'");
+    }
+
+    //метод для аутентификации
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Employee not found"));
+        return user;
+    }
+}
