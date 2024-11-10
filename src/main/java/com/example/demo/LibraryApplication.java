7ba2065ac9ff6c2d7cbbc2056c6eb669fa4087f7
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-11-10 23:24:46 +0300

    add EnableConfigurationProperties

--- src/main/java/com/example/demo/LibraryApplication.java
+++ src/main/java/com/example/demo/LibraryApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.component.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class LibraryApplication {
 
 	public static void main(String[] args) {
