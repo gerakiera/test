6c98ddc1e10363034b35e9f8b13117dbbe9588ae
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-11-10 23:25:31 +0300

    add userUtils

---
+++ src/main/java/com/example/demo/util/UserUtils.java
@@ -0,0 +1,27 @@
+package com.example.demo.util;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserUtils {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Bean
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        var login = authentication.getName();
+        return userRepository.findByLogin(login).get();
+    }
+}
